112f2b2797287312f76eb3a2839c4b7844c1b726
vipulvikram <b516062@iiit-bh.ac.in>
2019-07-11 21:23:35 +0530

    copy

---
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that list my todos
+    @app.route('/shivang')
+    def shivang():
+        return ('Wake Up' + '<br/>' +
+            'Drink Coffee' + '<br/>' +
+            'Read Non-fiction Novel' + '<br/>'
+        )
+
+    return app
+
