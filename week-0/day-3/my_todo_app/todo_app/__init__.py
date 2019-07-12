4c34531658c27b095e6880db75af50cb4ff24fad
vipulvikram <b516062@iiit-bh.ac.in>
2019-07-12 23:23:57 +0530

    final

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask
 
 
 def create_app(test_config=None):
@@ -12,38 +12,6 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    def todo_view(todo):
-        the_view = 'List of my todos:' + '<br/>'
-        for to in todo:
-            the_view += to +'<br/>'
-        the_view += '__LIST ENDS__'
-        return the_view;
-
-    def functio(name):
-        if name == 'depo':
-            return ['Go for run', 'ohyes']
-        else:
-            return ['vipul', 'vikram']
-
-
-    @app.route('/vip')
-    def vip():
-        name = request.args.get('name')
-        print(name)
-        my_todos = functio(name)
-        
-        return todo_view(my_todos)
-
-
-    @app.route('/raj')
-    def raj():
-        return render_template('index.html')
-
-    @app.route('/depo')
-    def depo():
-        todo = ['Go for run', 'ohyes']
-        return todo_view(todo)
 
     # a simple page that list my todos
     @app.route('/shivang')
